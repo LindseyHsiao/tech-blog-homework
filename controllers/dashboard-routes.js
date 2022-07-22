9744e1a409ce1f2f564f6a16737bd4da7a6f1b78
Lindsey Sherrill <lindsey350@aol.com>
2022-07-22 10:26:00 -0400

    added dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -22,5 +22,44 @@ router.use('/', withAuth, (req, res)=>{ //why not router.get?
 //New Route for new posts needs to have a handlebar form for submit and (js)
 
 //Edit Route for editing your posts and handlebar form for submit (js)
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id, { //can these be deleted?
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+        
+        res.render('edit-post', {
+          post,
+          loggedIn: true
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports =  router;
